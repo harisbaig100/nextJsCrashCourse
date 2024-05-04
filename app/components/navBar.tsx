525e155e426bf3fec23a5e15c8552574e25ae33c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-05-04 22:42:20 +0500

    styles

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import Logo from './dojo-logo.png'
 
 const NavBar = () => {
     return (
         <nav>
+            <Image src={Logo} alt="logo" width={70} height={100} placeholder='blur' quality={100} />
             <Link href="/">Dashboard</Link>
             <Link href="/tickets">Tickets</Link>
         </nav>
